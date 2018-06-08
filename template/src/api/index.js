d80bd88932db5c9e75e5732a7e655756b3415a22
lcqiong <417783524@qq.com>
2018-06-08 14:20:21 +0800

    template

--- template/src/api/index.js
+++ template/src/api/index.js
@@ -10,28 +10,29 @@ const $axios = axios.create({
   withCredentials: false
 })
 
-// 定义一个响应状态，响应成功以及各种失败的情况
-// const requestStatus = {
-//   ok: 0,
-//   fail: 1,
-//   cancel: -1
-// }
+// 定义一个响应状态，响应成功以及各种响应失败的情况
+const requestStatus = {
+  ok: 0, // 响应成功的状态
+  fail: 1, // 响应fail的情况
+  cancel: -1 // 可能是验证码失效或者其他因素造成的
+}
 
 $axios.interceptors.request.use(function (config) {
-  // Do something before request is sent
+  // 若请求头需要添加参数，可直接在此处设置config(直接添加（以对象的形式）)
   return config
 }, function (error) {
   // Do something with request error
   return Promise.reject(error)
 })
 
-// Add a response interceptor
+// Add a response interceptors
 $axios.interceptors.response.use(function (response) {
-  // Do something with response data
+  // 请求成功
   console.log(response)
   return response
 }, function (error) {
-  // Do something with response error
+  // 请求出错的时候做些什么
+  console.log('请求出错', error)
   return Promise.reject(error)
 })
 
